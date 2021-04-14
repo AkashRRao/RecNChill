9f2ab89e2e540cac0b548b671dd2ff90ea717f04
Akash Rao <akashrrao@gmail.com>
2021-04-14 18:36:18 -0500

    Create feedback data from client side for movie cards

--- public/card-stack/card-stack.js
+++ public/card-stack/card-stack.js
@@ -1,4 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
+let feedbackData = {
+  userId: '1234',
+  moviesLiked: new Set(),
+  moviesDisliked: new Set(),
+};
+
+let cards = new Set();
+
+function submitMovieFeedback() {
+  cards.forEach((card) => card.destroy());
+  console.log(`user ${feedbackData.userId} has submitted movie feedback`);
+  console.log(
+    `here are the movies liked ${Array.from(feedbackData.moviesLiked)}`
+  );
+  console.log(
+    `and here are the movies disliked ${Array.from(
+      feedbackData.moviesDisliked
+    )}`
+  );
+  let submitDiv = document.getElementById('submit-feedback');
+  submitDiv.querySelector('button').innerText = 'Loading...';
+  submitDiv.querySelector('button').disabled = true;
+  alert('check console');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
   var Direction = window.swing.Direction;
 
   const config = {
@@ -11,28 +36,37 @@ document.addEventListener("DOMContentLoaded", function () {
   let stack;
   stack = window.swing.Stack(config);
 
-  [].forEach.call(document.querySelectorAll(".card"), function (targetElement) {
+  [].forEach.call(document.querySelectorAll('.card'), function (targetElement) {
     stack.createCard(targetElement);
   });
 
-  stack.on("throwout", function (e) {
-    console.log(
-      e.target.innerText || e.target.textContent,
-      "has been thrown out of the stack to the",
-      e.throwDirection,
-      "direction."
-    );
-    const card = stack.getCard(e.target);
-    console.log(card);
-    // card.destroy();
+  stack.on('throwout', function (e) {
+    const movieId = e.target.querySelector('img').id;
+    if (e.throwDirection == Direction.RIGHT) {
+      // movieId liked
+      if (feedbackData.moviesDisliked.has(movieId)) {
+        feedbackData.moviesDisliked.delete(movieId);
+      }
+      feedbackData.moviesLiked.add(movieId);
+    } else {
+      // movieId disliked
+      if (feedbackData.moviesLiked.has(movieId)) {
+        feedbackData.moviesLiked.delete(movieId);
+      }
+      feedbackData.moviesDisliked.add(movieId);
+    }
+    cards.add(stack.getCard(e.target));
   });
 
-  stack.on("throwin", function (e) {
-    console.log(
-      e.target.innerText || e.target.textContent,
-      "has been thrown into the stack from the",
-      e.throwDirection,
-      "direction."
-    );
+  stack.on('throwin', function (e) {
+    const movieId = e.target.querySelector('img').id;
+    if (e.throwDirection == Direction.RIGHT) {
+      // movieId was liked
+      feedbackData.moviesLiked.delete(movieId);
+    } else {
+      // movieId was disliked
+      feedbackData.moviesDisliked.delete(movieId);
+    }
+    cards.add(stack.getCard(e.target));
   });
 });
